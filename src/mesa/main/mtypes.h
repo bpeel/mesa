09d2d097c3f80f2cc4f418406fac8c5dd5089b7e
Ilia Mirkin <imirkin@alum.mit.edu>
2014-03-01 16:46:53 -0500

    mesa/main: add ARB_clear_texture entrypoints
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Reviewed-by: Neil Roberts <neil@linux.intel.com>

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -3508,6 +3508,7 @@ struct gl_extensions
    GLboolean ARB_base_instance;
    GLboolean ARB_blend_func_extended;
    GLboolean ARB_buffer_storage;
+   GLboolean ARB_clear_texture;
    GLboolean ARB_color_buffer_float;
    GLboolean ARB_compute_shader;
    GLboolean ARB_conservative_depth;
